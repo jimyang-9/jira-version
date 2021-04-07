8366ed9e8026dafc3722a5ab266a8ffb910680d3
Jim Yang <jim.yang@onepeloton.com>
2021-04-07 12:26:52 -0400

    WIP main

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {getAllJiraVersions, getLatestUnreleasedVersion} from './client'
+import {SUBDOMAIN, PROJECT, EMAIL, API_TOKEN} from './env'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const versions = await getAllJiraVersions(
+      SUBDOMAIN,
+      PROJECT,
+      EMAIL,
+      API_TOKEN
+    )
+    core.debug(`versions: ${versions}`)
+    const latestVersion = getLatestUnreleasedVersion(versions)
+    if (!latestVersion) {
+      // TODO: is this okay?
+      core.setFailed('Could not find latest unreleased version')
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('latest-version', latestVersion)
   } catch (error) {
     core.setFailed(error.message)
   }
